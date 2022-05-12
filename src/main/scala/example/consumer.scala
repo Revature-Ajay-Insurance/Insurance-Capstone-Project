93cb1fa9615ac723c19d469a2749f28c694c00aa
itimeye <77209646+itimeye@users.noreply.github.com>
2022-05-12 09:31:13 -0500

    EC2 changes

--- src/main/scala/example/consumer.scala
+++ src/main/scala/example/consumer.scala
@@ -1,3 +1,5 @@
+// spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.4 consumer.jar --class example.consumer
+// spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0 ecommerconsumer.jar
 package example
 
 import java.util.{Collections, Properties}
@@ -13,75 +15,76 @@ import org.apache.spark.sql.types._
 import org.apache.spark.sql.{Column, Dataset, Row, SparkSession}
 
 object consumer{
-    // def main(args: Array[String]):Unit = {
-    //     // Output Data to Kafka create new Key Value Pairs - Topic Insurance
-    //   // Create Spark Session to Stream to Kafka
-    //   val spark = SparkSession.builder
-    //   .appName("KafkaSource")
-    //   .config("spark.master", "local[*]")
-    //   .getOrCreate()
-
-    //   import spark.implicits._
-    //   // Set Spark logging level to ERROR.
-    //   // spark.sparkContext.setLogLevel("ERROR")
-    //   val initDf = spark.readStream
-    //     .format("kafka")
-    //     .option("kafka.bootstrap.servers", "sandbox-hdp.hortonworks.com:6667")
-    //     .option("subscribe", "insurance")
-    //     .load()
-    //     .select(col("value").cast("string"))
-
-    //     // Data Transformations/aggregations
-    //   val resultDf = initDf.withColumn("value", 
-    //     concat_ws(",",col("customer_id"),col("customer_name"),col("customer_age"),col("agent_id"),col("agent_name"),col("claim_category"),col("amount"),col("reason"),col("agent_rating"),col("datetime"),col("country"),col("state"),col("approval"), col("reimbursement_id"), col("failure_reason")))
-    //     // .withColumn("key", col("claim_id"))
-
-    //     // Write Data transformations into a new topic
-    //   resultDf.selectExpr("CAST(claim_id AS STRING) AS key", "CAST(value AS STRING) AS value")
-    //     .writeStream
-    //     .format("kafka")
-    //     .option("kafka.bootstrap.servers", "localhost:9092")
-    //     .option("kafka.bootstrap.servers", "sandbox-hdp.hortonworks.com:6667")
-    //     .option("topic", "insuranceclean")
-    //     .option("checkpointLocation", "file:///home/maria_dev/output/checkpoint/kafka_checkpoint")
-    //     .start()
-    //     .awaitTermination()
-    // }
-
     def main(args: Array[String]):Unit = {
-        val props: Properties = new Properties()
-        props.put("group.id", "test-consumer-group") //need group id from
-        props.put("bootstrap.servers", "sandbox-hdp.hortonworks.com:6667")
-        props.put(
-        "key.deserializer",
-        "org.apache.kafka.common.serialization.StringDeserializer"
-        )
-        props.put(
-        "value.deserializer",
-        "org.apache.kafka.common.serialization.StringDeserializer"
-        )
-        props.put("enable.auto.commit", "true")
-        props.put("auto.commit.interval.ms", "2000")
-        val consumer = new KafkaConsumer(props)
-        val topics = List("insurance")
-        try {
-        consumer.subscribe(topics.asJava) //subscribe to topic as a type Java
-        while (true) {
-            val records = consumer.poll(10) //read one record every 10 seconds
-            for (record <- records.asScala) { // convert the read records into Scala code
-            println( //for each record print Topic, Key, Value, Offset, Partiton
-                // "Topic: " + record.topic() +
-                ",Key: " + record.key() +
-                ",Value: " + record.value() +
-                // ", Offset: " + record.offset() +
-                ", Partition: " + record.partition()
-            )
-            }
-        }
-        } catch {
+        // Output Data to Kafka create new Key Value Pairs - Topic Insurance
+      // Create Spark Session to Stream to Kafka
+      val spark = SparkSession.builder
+      .appName("KafkaSource")
+      .config("spark.master", "local[*]")
+      .getOrCreate()
+
+      import spark.implicits._
+      // Set Spark logging level to ERROR.
+      spark.sparkContext.setLogLevel("ERROR")
+        
+
+      // Define Schema for Dataframe being created
+      try{
+      val schema = StructType(
+      List(
+        StructField("order_id", StringType, true),
+        StructField("customer_id", StringType, true),
+        StructField("customer_name", StringType, true),
+        StructField("product_id", StringType, true),
+        StructField("product_name", StringType, true),
+        StructField("product_category", StringType, true),
+        StructField("payment_type", StringType, true),
+        StructField("qty", StringType, true),
+        StructField("price", StringType, true),
+        StructField("datetime", StringType, true),
+        StructField("country", StringType, true),
+        StructField("city", StringType, true),
+        StructField("ecommerce_website_name", StringType, true),
+        StructField("payment_txn_id", StringType, true),
+        StructField("payment_txn_success", StringType, true),
+        StructField("failure_reason", StringType, true)
+      )
+    )
+      // reading from insurance topic in Kafka
+      val initDf = spark.readStream
+        .format("kafka")
+        .option("kafka.bootstrap.servers", "localhost:9092")
+        .option("subscribe", "insurance")
+        .load()
+        .select(col("value").cast("string"))
+        
+        // put data into schema
+        val copyDF = initDf.selectExpr("CAST (value AS STRING)").toDF("value")
+        copyDF.printSchema()
+
+        val cleanDf = copyDF.select(from_json(col("value"), schema))
+        cleanDf.printSchema()
+
+        // // Output to console
+        // cleanDf.writeStream
+        //     .outputMode("update")
+        //     .format("console")
+        //     .start()
+        //     .awaitTermination()
+        // Output to CSV file
+        cleanDf.writeStream
+            .outputMode("append") // Filesink only support Append mode.
+            .format("csv") // supports these formats : csv, json, orc, parquet
+            .option("path", "file:///home/hdoop/output/synafire_output")
+            .option("header", false)
+            .option("checkpointLocation", "file:///home/hdoop/output/checkpoint/synafire_checkpoint")
+            .start()
+            .awaitTermination()
+        
+      } catch {
         case e: Exception => e.printStackTrace()
-        } finally {
-        consumer.close()
-        }
+      } finally {
+        spark.close()
+      }
     }
-} 
+}
